15d5e9245757e51ca2ed937aa732f27b0a029561
Deniz <deniz.turel@gmail.com>
2023-02-23 11:15:32 +0300

    instance eklendi

--- src/gipOpenAI.cpp
+++ src/gipOpenAI.cpp
@@ -6,6 +6,8 @@
  */
 
 #include "gipOpenAI.h"
+#include "gFile.h"
+#include "gImage.h"
 
 
 gipOpenAI::gipOpenAI() {
@@ -25,7 +27,79 @@ gipOpenAI::~gipOpenAI() {
 
 void gipOpenAI::initialize(std::string apiKey) {
 	apikey = apiKey;
-	openai::start(apikey);
+	openai::start();
+	setActiveInstance();
+}
+
+//void gipOpenAI::createNewInstance(openai::OpenAI& openai) {
+//    // You can try catch API method if an error occur
+//    try {
+//        auto res = openai.completion.create({
+//            {"model", "text-davinci-003"},
+//            {"prompt", "Say bar() function called"}
+//        });
+//        std::cout << res << "\n\n";
+//    }
+//    catch(std::exception const& e) {
+//        std::cerr << "Exception:" <<  e.what() << "\n\n";
+//    }
+//}
+//
+
+std::string gipOpenAI::createInstance(openai::OpenAI& openai, std::string prompt, int maxTokens, int modelType, int temperature) {
+//	obje.getTextCompletion(prompt, 500);
+
+	Json j = {
+				{"model", modeltype[modelType]},
+				{"prompt", prompt},
+				{"max_tokens", maxTokens},
+				{"temperature", temperature}
+		};
+		auto completion = openai.completion.create(j);
+	//	std::cout << "Response is:\n" << completion.dump(2) << '\n';
+//		std::cout << completion["choices"][0]["text"] << "\n";
+		return completion["choices"][0]["text"];
+}
+
+std::string gipOpenAI::createNewInstance(openai::OpenAI& openai) {
+
+    // You can try catch API method if an error occur
+	Json j = {{"model", "text-davinci-003"},{"prompt", "say AAAAAAAA!!!!!! then give 5 random girl names "},{"max_tokens", 500}};
+    try {
+        auto completion = openai.completion.create(j);
+        std::cout << completion["choices"][0]["text"] << "\n";
+        return completion["choices"][0]["text"];
+    }
+    catch(std::exception const& e) {
+        std::cerr << "Exception:" <<  e.what() << "\n\n";
+    }
+}
+
+void gipOpenAI::setActiveInstance() {
+	// Create openai instance
+//	openai_instance = openai::OpenAI{"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"};
+	openai::OpenAI openai_instance{"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"};
+//	openai::OpenAI openai_instance{};
+	createNewInstance(openai_instance);
+
+	// You can create other openai instances with different tokens and parameters
+	{
+		openai::OpenAI another_openai_instance{"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"};
+		// another_openai_instance.setThrowException(false); // You can set throw exception to false if you want
+
+		try {
+			auto completion = another_openai_instance.completion.create({
+				{"model", "text-davinci-003"},
+				{"prompt", "Say this should throw since token is invalid here"}
+			});
+//			std::cout << completion["choices"][0]["text"] << "\n";
+		}
+		catch(std::exception const& e) {
+			std::cerr << "02-basic failed purposely because of " << e.what() << "\n\n";
+		}
+
+	}
+
 }
 
 std::string gipOpenAI::getTextCompletion(std::string prompt, int maxTokens, int modelType, int temperature) {
@@ -65,40 +139,44 @@ std::vector<std::string> gipOpenAI::getImage(std::string prompt, int num, std::s
 	Json j = {
 			{"prompt", prompt},
 			{"n", num},
-			{"size", size}
-	};
-    auto image = openai::image().create(j); // Using initializer lists
-//    std::cout << "Image URL is: " << image.dump(2) << '\n';
-    std::vector<std::string> urls;
-    for(int i = 0; i < num; i++) urls.push_back(image["data"][i]["url"]);
-    return urls;
-}
-
-std::vector<std::string> gipOpenAI::getImageBase64(std::string prompt, int num, std::string size) {
-	Json j = {
-			{"prompt", prompt},
-			{"n", num},
 			{"size", size},
 			{"response_format", "b64_json"}
 	};
     auto image = openai::image().create(j); // Using initializer lists
     std::vector<std::string> data;
-    for(int i = 0; i < num; i++) data.push_back(image["data"][i]["b64_json"]);
+    for(int i = 0; i < num; i++) {
+    	gFile ifile;
+    	ifile.load(gImage::generateDownloadedImagePath(), gFile::FILEMODE_WRITEONLY, true);
+    	ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
+    	data.push_back(ifile.getPath().string());
+    }
     return data;
 }
 
-std::vector<std::string> gipOpenAI::getImageVariation(std::string imageDataBase64, int num, std::string size) {
+std::vector<std::string> gipOpenAI::getImageVariation(std::string imageFullPath, int num, std::string size) {
+	gFile ifileo;
+	ifileo.load(imageFullPath, gFile::FILEMODE_READONLY, true);
+	std::vector<char> ibytes = ifileo.getBytes();
+//	gLogi("OAI") << "image fullpath:" << ifileo.getPath().string();
+//	gLogi("OAI") << "bytes:" << gEncodeBase64(ibytes.data(), ibytes.size());
 	Json j = {
-			{"image", imageDataBase64},
+			{"image", gEncodeBase64(ibytes.data(), ibytes.size())},
 			{"n", num},
-			{"size", size}
+			{"size", size},
+			{"response_format", "b64_json"}
 	};
+	ifileo.close();
 
     auto image = openai::image().variation(j);
-//    std::cout << "Image URL is: " << image.dump(2) << '\n';
-    std::vector<std::string> urls;
-    for(int i = 0; i < num; i++) urls.push_back(image["data"][i]["url"]);
-    return urls;
+    std::vector<std::string> data;
+    for(int i = 0; i < num; i++) {
+    	gFile ifile;
+    	ifile.load(gImage::generateDownloadedImagePath(), gFile::FILEMODE_WRITEONLY, true);
+    	ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
+    	data.push_back(ifile.getPath().string());
+    	ifile.close();
+    }
+    return data;
 }
 
 std::string gipOpenAI::getCode(std::string prompt, int maxTokens, int modelType) {
@@ -111,3 +189,15 @@ std::string gipOpenAI::getCode(std::string prompt, int maxTokens, int modelType)
 //	std::cout << "Response is:\n" << completion.dump(2) << '\n';
 	return completion["choices"][0]["text"];
 }
+
+std::string gipOpenAI::getCodeDeneme(openai::OpenAI& openai, std::string prompt, int maxTokens, int modelType) {
+	Json j = {
+			{"model", modeltype[modelType]},
+			{"max_tokens", maxTokens},
+			{"prompt", prompt}
+	};
+	auto completion = openai.completion.create(j);
+//	std::cout << "Response is:\n" << completion.dump(2) << '\n';
+	return completion["choices"][0]["text"];
+}
+
--- src/gipOpenAI.h
+++ src/gipOpenAI.h
@@ -13,6 +13,8 @@
 
 
 class gipOpenAI : public gBaseComponent {
+
+	openai::OpenAI openai_instance;
 public:
 
 	enum {
@@ -28,21 +30,31 @@ public:
 	virtual ~gipOpenAI();
 
 	void initialize(std::string apiKey);
+	void createNewInstance(std::string prompt, int maxTokens, int modelType, int temperature);
+	void setActiveInstance();
+	std::string createNewInstance(openai::OpenAI& openai);
+	std::string createInstance(openai::OpenAI& openai, std::string prompt, int maxTokens, int modelType = MODEL_TEXT_DAVINCI, int temperature = 0);
 
 	std::string getTextCompletion(std::string prompt, int maxTokens, int modelType = MODEL_TEXT_DAVINCI, int temperature = 0);
 	std::string getTextEdit(std::string input, std::string instruction, int modelType = MODEL_TEXTEDIT_DAVINCI);
 	std::string getTextEmbed(std::string input, int modelType = MODEL_TEXTEMBED_ADA);
 
 	std::vector<std::string> getImage(std::string prompt, int num = 1, std::string size = "256x256");
-	std::vector<std::string> getImageBase64(std::string prompt, int num = 1, std::string size = "256x256");
-	std::vector<std::string> getImageVariation(std::string imageDataBase64, int num = 1, std::string size = "256x256");
+	std::vector<std::string> getImageVariation(std::string imageFullPath, int num = 1, std::string size = "256x256");
 
 	std::string getCode(std::string prompt, int maxTokens, int modelType = MODEL_CODE_DAVINCI);
+	std::string getCodeDeneme(openai::OpenAI& openai, std::string prompt, int maxTokens, int modelType = MODEL_CODE_DAVINCI);
 
 
 private:
 	std::string apikey;
 	std::string modeltype[8];
+	std::string str;
+//	std::reference_wrapper<openai::OpenAI> openai_;
+//	openai::OpenAI openai_instance;
+
+
 };
+
 
 #endif /* SRC_GIPOPENAI_H_ */
