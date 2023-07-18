7c6122fc788940dab0c068ee104523ca35e3d37d
SAGLAMER <SAGLAMER>
2023-07-18 11:42:03 +0300

    Fine-tuning model created

--- src/gipOpenAI.cpp
+++ src/gipOpenAI.cpp
@@ -9,7 +9,6 @@
 #include "gFile.h"
 #include "gImage.h"
 
-
 gipOpenAI::gipOpenAI() {
 	apikey = "";
 	modeltype[MODEL_CHAT_GPT40] = "gpt-4";
@@ -32,99 +31,99 @@ void gipOpenAI::initialize(std::string apiKey) {
 	openai::start(apikey);
 }
 
-std::string gipOpenAI::getChatCompletion(std::string prompt, int maxTokens, int modelType, int temperature) {
-	Json messages = Json::array( {Json::object({{"role", "user"}, {"content", prompt}})} );
-	Json j = {
-			{"model", modeltype[modelType]},
-			{"messages", messages},
-			{"max_tokens", maxTokens},
-			{"temperature", temperature}
-	};
+std::string gipOpenAI::getChatCompletion(std::string prompt, int maxTokens,
+		int modelType, int temperature) {
+	Json messages = Json::array( { Json::object( { { "role", "user" }, {
+			"content", prompt } }) });
+	Json j = { { "model", modeltype[modelType] }, { "messages", messages }, {
+			"max_tokens", maxTokens }, { "temperature", temperature } };
 	auto completion = openai::chat().create(j);
 //	std::cout << "Response is:\n" << completion.dump(2) << '\n';
 	return completion["choices"][0]["message"]["content"];
 }
 
-std::string gipOpenAI::getTextCompletion(std::string prompt, int maxTokens, int modelType, int temperature) {
-	Json j = {
-			{"model", modeltype[modelType]},
-			{"prompt", prompt},
-			{"max_tokens", maxTokens},
-			{"temperature", temperature}
-	};
+std::string gipOpenAI::getTextCompletion(std::string prompt, int maxTokens,
+		int modelType, int temperature) {
+	Json j = { { "model", modeltype[modelType] }, { "prompt", prompt }, {
+			"max_tokens", maxTokens }, { "temperature", temperature } };
 	auto completion = openai::completion().create(j);
 //	std::cout << "Response is:\n" << completion.dump(2) << '\n';
 	return completion["choices"][0]["text"];
 }
 
-std::string gipOpenAI::getTextEdit(std::string input, std::string instruction, int modelType) {
-	Json j = {
-			{"model", modeltype[modelType]},
-			{"input", input},
-			{"instruction", instruction}
-	};
+std::string gipOpenAI::getTextEdit(std::string input, std::string instruction,
+		int modelType) {
+	Json j = { { "model", modeltype[modelType] }, { "input", input }, {
+			"instruction", instruction } };
 	auto edit = openai::edit().create(j);
 //	std::cout << "Response is:\n" << edit.dump(2) << '\n';
 	return edit["choices"][0]["text"];
 }
 
 std::string gipOpenAI::getTextEmbed(std::string input, int modelType) {
-	Json j = {
-			{"model", modeltype[modelType]},
-			{"input", input}
-	};
+	Json j = { { "model", modeltype[modelType] }, { "input", input } };
 	auto res = openai::embedding().create(j);
 	std::cout << "Response is:\n" << res.dump(2) << '\n';
 	return res.dump(2);
 }
 
-std::vector<std::string> gipOpenAI::getImage(std::string prompt, int num, std::string size) {
-	Json j = {
-			{"prompt", prompt},
-			{"n", num},
-			{"size", size},
-			{"response_format", "b64_json"}
-	};
-    auto image = openai::image().create(j); // Using initializer lists
-    std::vector<std::string> data;
-    for(int i = 0; i < num; i++) {
-    	gFile ifile;
-    	ifile.load(gImage::generateDownloadedImagePath(), gFile::FILEMODE_WRITEONLY, true);
-    	ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
-    	data.push_back(ifile.getPath().string());
-    }
-    return data;
+std::vector<std::string> gipOpenAI::getImage(std::string prompt, int num,
+		std::string size) {
+	Json j = { { "prompt", prompt }, { "n", num }, { "size", size }, {
+			"response_format", "b64_json" } };
+	auto image = openai::image().create(j); // Using initializer lists
+	std::vector < std::string > data;
+	for (int i = 0; i < num; i++) {
+		gFile ifile;
+		ifile.load(gImage::generateDownloadedImagePath(),
+				gFile::FILEMODE_WRITEONLY, true);
+		ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
+		data.push_back(ifile.getPath().string());
+	}
+	return data;
 }
 
-std::vector<std::string> gipOpenAI::getImageVariation(std::string imageFullPath, int num, std::string size) {
-	Json j = {
-			{"image", imageFullPath},
-			{"n", num},
-			{"size", size},
-			{"response_format", "b64_json"}
-	};
+std::vector<std::string> gipOpenAI::getImageVariation(std::string imageFullPath,
+		int num, std::string size) {
+	Json j = { { "image", imageFullPath }, { "n", num }, { "size", size }, {
+			"response_format", "b64_json" } };
 
-    auto image = openai::image().variation(j);
+	auto image = openai::image().variation(j);
 //    std::cout << "Image response:\n" << image.dump(2) << '\n';
 
-    std::vector<std::string> data;
-    for(int i = 0; i < num; i++) {
-    	gFile ifile;
-    	ifile.load(gImage::generateDownloadedImagePath(), gFile::FILEMODE_WRITEONLY, true);
-    	ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
-    	data.push_back(ifile.getPath().string());
-    	ifile.close();
-    }
-    return data;
+	std::vector < std::string > data;
+	for (int i = 0; i < num; i++) {
+		gFile ifile;
+		ifile.load(gImage::generateDownloadedImagePath(),
+				gFile::FILEMODE_WRITEONLY, true);
+		ifile.write(gDecodeBase64(image["data"][i]["b64_json"]));
+		data.push_back(ifile.getPath().string());
+		ifile.close();
+	}
+	return data;
 }
 
-std::string gipOpenAI::getCode(std::string prompt, int maxTokens, int modelType) {
-	Json j = {
-			{"model", modeltype[modelType]},
-			{"max_tokens", maxTokens},
-			{"prompt", prompt}
-	};
+std::string gipOpenAI::getCode(std::string prompt, int maxTokens,
+		int modelType) {
+	Json j = { { "model", modeltype[modelType] }, { "max_tokens", maxTokens }, {
+			"prompt", prompt } };
 	auto completion = openai::completion().create(j);
 //	std::cout << "Response is:\n" << completion.dump(2) << '\n';
+	return completion["choices"][0]["text"];
+}
+
+std::string gipOpenAI::fineTuneModel(std::string file) {
+	auto fineTune = openai::fineTune().create( { { "training_file", file }, { "model", "davinci" }, { "n_epochs", 15 }, { "batch_size", 3 }, { "learning_rate_multiplier", 0.3 }, });
+	return fineTune["id"].get<std::string>();
+}
+
+gipOpenAI::Json gipOpenAI::retrieveFineTunedModelContent(std::string fineTuneId) {
+	return openai::fineTune().retrieve(fineTuneId);
+}
+
+std::string gipOpenAI::getCompletion(std::string prompt, int maxTokens, std::string modelType, int temperature) {
+	Json j = { { "model", modelType }, { "prompt", prompt }, { "max_tokens", maxTokens }, { "temperature", temperature } };
+	auto completion = openai::completion().create(j);
+	std::cout << "Response is:\n" << completion.dump(2) << '\n';
 	return completion["choices"][0]["text"];
 }
--- src/gipOpenAI.h
+++ src/gipOpenAI.h
@@ -11,16 +11,20 @@
 #include "gBaseComponent.h"
 #include "openai.hpp"
 
-
-class gipOpenAI : public gBaseComponent {
+class gipOpenAI: public gBaseComponent {
 public:
 
 	enum {
-		MODEL_CHAT_GPT40, MODEL_CHAT_GPT35TURBO,
-		MODEL_TEXT_DAVINCI, MODEL_TEXT_CURIE, MODEL_TEXT_BABBAGE, MODEL_TEXT_ADA,
+		MODEL_CHAT_GPT40,
+		MODEL_CHAT_GPT35TURBO,
+		MODEL_TEXT_DAVINCI,
+		MODEL_TEXT_CURIE,
+		MODEL_TEXT_BABBAGE,
+		MODEL_TEXT_ADA,
 		MODEL_TEXTEDIT_DAVINCI,
 		MODEL_TEXTEMBED_ADA,
-		MODEL_CODE_DAVINCI, MODEL_CODE_CUSHMAN
+		MODEL_CODE_DAVINCI,
+		MODEL_CODE_CUSHMAN
 	};
 
 	typedef nlohmann::json Json;
@@ -40,6 +44,32 @@ public:
 	std::vector<std::string> getImageVariation(std::string imageFullPath, int num = 1, std::string size = "256x256");
 
 	std::string getCode(std::string prompt, int maxTokens, int modelType = MODEL_CODE_DAVINCI);
+	/*
+	 * Fine tunes a model using the specified file.
+	 *
+	 * @param file The file to be used for fine tuning.
+	 * @return The result of the fine tuning process.
+	 */
+	std::string fineTuneModel(std::string file);
+
+	/*
+	 * Retrieves the content of a fine-tuned model with a given ID.
+	 *
+	 * @param fineTuneId The ID of the fine-tuned model.
+	 * @return A Json object containing the content of the fine-tuned model.
+	 */
+	Json retrieveFineTunedModelContent(std::string fineTuneId);
+
+	/*
+	 * Get a completion of a given prompt using a specific model type.
+	 *
+	 * @param prompt The initial string to which the model should respond.
+	 * @param maxTokens The maximum length of the model’s response.
+	 * @param modelType The type of the model to be used for generating the completion.
+	 * @param temperature (Optional) The randomness of the model’s output. Default is 0.
+	 * @return The generated completion as a string.
+	 */
+	std::string getCompletion(std::string prompt, int maxTokens, std::string modelType, int temperature = 0);
 
 
 private:
